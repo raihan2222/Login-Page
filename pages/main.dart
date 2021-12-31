73005065cfbbaf7de259eb6c98a96f07316c8e20
Raihan <abu2222raihan@gmail.com>
2021-12-31 22:44:55 +0600

    Lognin Page

---
+++ pages/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'login_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      routes: {
+        "/login_page": (context) => LoginPage(),
+      },
+    );
+  }
+}
